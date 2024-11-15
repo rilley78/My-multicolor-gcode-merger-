4b0ac3dd5d9bb5f1a63327e7bfd9b340085fd705
Rilley78 <rilley78@mail.ru>
2024-11-15 13:52:11 +0300

    opens and saves files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,54 @@
-#include <fmt/core.h>
+#include <nfd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <filesystem>
+#include <string>
 
-int main()
+int main( void )
 {
-    fmt::print("Hello World!\n");
+    int colorcount;
+    std::cout << "Enter number of colours\n";
+    std::cin >> colorcount;
+
+    NFD_Init();
+    nfdu8char_t* outPath;
+    nfdu8filteritem_t filters[1] = { { "Gcodes", "gcode" } };
+    nfdopendialogu8args_t args = { 0 };
+    args.filterList = filters;
+    args.filterCount = 1;
+    std::ifstream fileIn;
+    std::ofstream fileOut {"test.gcode"};
+    for (int i = 0; i < colorcount; i++) 
+    {    
+        nfdresult_t result = NFD_OpenDialogU8_With(&outPath, &args);
+        if (result == NFD_OKAY) 
+        {
+            std::cout << "File #" << colorcount << " opened successfully!\n";
+            fileIn.open(outPath);
+            NFD_FreePathU8(outPath);
+            std::istreambuf_iterator<char> begin_file(fileIn);
+            std::copy(std::istreambuf_iterator<char>(fileIn), 
+            std::istreambuf_iterator<char>(), 
+            std::ostreambuf_iterator<char>(fileOut));
+            fileIn.close();
+        }
+        else if (result == NFD_CANCEL) 
+        {
+            puts("User pressed cancel.");
+            break;
+        }
+        else 
+        {
+            printf("Error: %s\n", NFD_GetError());
+            break;
+        }
+    }
+    fileOut.close();
+
+    NFD_Quit();
     return 0;
 }
